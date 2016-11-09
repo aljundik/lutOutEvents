defc0805d5d4175fb9ef4a65d6de75ab6f98a837
aljundik <kaledj007@gmail.com>
2016-11-09 18:47:12 +0200

    add student and custom event end points

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var ctrlOrganizer = require('../controllers/organizer.controllers.js');
 var ctrlEvent = require('../controllers/event.controllers.js');
+var ctrlStudent = require('../controllers/student.controllers.js');
+var ctrlCustomEvent = require('../controllers/customEvent.controllers.js');
 
 router
 	.route('/organizer') 	
@@ -15,7 +17,7 @@ router
 	.put(ctrlOrganizer.organizerEdit)
 	.delete(ctrlOrganizer.organizerDelete);
 
-// events routes
+// Organizer events routes
 router
 	.route('/organizer/:organizerId/event')
 	.get(ctrlEvent.eventGetAll) 
@@ -25,5 +27,34 @@ router
 	.route('/organizer/:organizerId/event/:eventId')
 	.get(ctrlEvent.eventGetOne)
 	.put(ctrlEvent.eventUpdateOne)
-	.delete(ctrlEvent.eventDeleteOne);	
+	.delete(ctrlEvent.eventDeleteOne);
+
+
+// Student Routes 
+
+router
+	.route('/student')
+	.post(ctrlStudent.studentRegister)
+	.get(ctrlStudent.studentGetAll);
+
+router
+	.route('/student/:studentId')
+	.get(ctrlStudent.studentGetOne)
+	.put(ctrlStudent.studentEdit)
+	.delete(ctrlStudent.studentDelete);
+// Student Custom Event
+router
+	.route('/student/:studentId/customEvent')
+	.post(ctrlCustomEvent.customEventAddOne)
+	.get(ctrlCustomEvent.customEventGetAll);
+
+router
+	.route('/student/:studentId/customEvent/:customEventId')
+	.get(ctrlCustomEvent.customEventGetOne)
+	.put(ctrlCustomEvent.customEventUpdateOne)
+	.delete(ctrlCustomEvent.customEventDeleteOne);
+
+
+
+
 module.exports = router;
